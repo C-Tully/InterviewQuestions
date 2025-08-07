a5c29fdb3a774d407e295d5f257c6ba6de990dc8
Christopher Tully <christully12@gmail.com>
2025-08-07 11:38:47 -0400

    Update script.js
    Minor update to format.

--- Practice/Javascript/Promises/script.js
+++ Practice/Javascript/Promises/script.js
@@ -1,25 +1,42 @@
 //Goal: Fetch data from promisesData.json
 
-function fetchData() {
-  return fetch("data/promisesData.json")
-    .then((resp) => {
-      //inital network handling check
-      if (!resp.ok) {
-        throw new Error("Network response error.");
-      }
-      //Data successfuly fetched
-      return resp.json();
-    })
-    .then((data) => {
-      if (data.length > 0) {
-        console.log("Data!", data);
-        return data;
-      }
-    })
-    .catch((error) => {
-      //handle other error cases...
-      //Log error's handle if possible etc
-    });
-}
+async function fetchData() {
+  try {
+    const resp = await fetch("data/promisesData.json");
+    
+    if(!resp.ok) {
+      throw new Error("Network Response err");
+    }
 
+    const data = await resp.json();
+    if(data.length > 0) {
+      console.log('Data::', data)
+      return data;
+    } else {
+      throw new Error("No data found::");
+    }
+  } catch (err) {
+    console.error('Fetch Failed::', err.message);
+    throw err;
+  }
+  // return fetch("data/promisesData.json")
+  //   .then((resp) => {
+     
+  //     if (!resp.ok) {
+  //       throw new Error("Network response error.");
+  //     }
+  //     //Data successfuly fetched
+  //     return resp.json();
+  //   })
+  //   .then((data) => {
+  //     if (data.length > 0) {
+  //       console.log("Data!", data);
+  //       return data;
+  //     }
+  //   })
+  //   .catch((error) => {
+  //     //handle other error cases...
+  //     //Log error's handle if possible etc
+  //   });
+}
 fetchData();
